c05a0748fcfe54475cc3474daee4deaf16dcf4ba
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-06-04 00:05:20 +0700

    Edit data

--- js/core/data.js
+++ js/core/data.js
@@ -4,49 +4,56 @@ export const products = [
   {
     id: 1,
     name: "Woody Chair",
-    image: "https://kolik-128.webflow.io/product/woody-chair#",
+    image:
+      "https://assets-global.website-files.com/630c6fde459f67ef1a691e70/632c203e74d0effd6e7f4a75_LP%20006.jpg",
     price: 870.0,
     category: "Bestseller",
   },
   {
     id: 2,
     name: "Luna Chair",
-    image: "https://kolik-128.webflow.io/product/luna-chair#",
+    image:
+      "https://assets-global.website-files.com/630c6fde459f67ef1a691e70/632c20122e26ca23c49373ee_LP%20005.jpg",
     price: 850.0,
     category: "Bestseller",
   },
   {
     id: 3,
     name: "Coco",
-    image: "https://kolik-128.webflow.io/product/coco#",
+    image:
+      "https://assets-global.website-files.com/630c6fde459f67ef1a691e70/632c1f77033fb97c81a71623_LP%20001.jpg",
     price: 650.0,
     category: "Dinning Chairs",
   },
   {
     id: 4,
     name: "Pata",
-    image: "https://kolik-128.webflow.io/product/pata#",
+    image:
+      "https://assets-global.website-files.com/630c6fde459f67ef1a691e70/632c1f3c129a6ffe7d7f142a_LP%20004.jpg",
     price: 930.0,
     category: "Dinning Chairs",
   },
   {
     id: 5,
     name: "Osko",
-    image: "https://kolik-128.webflow.io/product/osko#",
+    image:
+      "https://assets-global.website-files.com/630c6fde459f67ef1a691e70/630e0e13dd57f2c466d716c7_shop%20image%2003%20(1).jpg",
     price: 899.0,
     category: "Bestseller",
   },
   {
     id: 5,
     name: "Ergo Chair",
-    image: "https://kolik-128.webflow.io/product/ergo-chair#",
+    image:
+      "https://assets-global.website-files.com/630c6fde459f67ef1a691e70/630e1f5ae9088b6cd88e5505_shop%20image%2007.jpg",
     price: 850.0,
     category: "Bestseller",
   },
   {
     id: 6,
     name: "Dory",
-    image: "https://kolik-128.webflow.io/product/dory#",
+    image:
+      "https://assets-global.website-files.com/630c6fde459f67ef1a691e70/630e0e428d845a70b03e6473_shop%20image%2002.jpg",
     price: 550.0,
     category: "Dinning Chairs",
   },
