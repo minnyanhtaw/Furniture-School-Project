d48d2088a09b26a475200ff414b753e4cdf7a30b
minnyanhtaw <minnyanhtaw07@gmail.com>
2024-06-04 18:43:46 +0700

    add third section

--- js/core/product.js
+++ js/core/product.js
@@ -2,9 +2,11 @@ import { homePageProductGroup, homePageProductTemplate } from "./selectors.js";
 
 export const createHomePageProduct = (product) => {
   const template = homePageProductTemplate.content.cloneNode(true);
+  template.querySelector(".homepage-product-card-link").href = product.link;
   template.querySelector(".homepage-product-image").src = product.image;
   template.querySelector(".homepage-product-name").innerText = product.name;
-  template.querySelector(".homepage-product-price").innerText = product.price.toFixed(2);
+  template.querySelector(".homepage-product-price").innerText =
+    product.price.toFixed(2);
   console.log(template);
   return template;
 };
